dc244f27dc7a7a574943b3166f63169aaad7a443
Travis <james.horine@gmail.com>
2024-08-08 09:21:23 -0500

    Provide for toggling diagnostics off and on

---
+++ config.lua
@@ -0,0 +1,25 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.leader=';'
+
+vim.g["diagnostics_active"] = true
+
+function Toggle_diagnostics()
+    if vim.g.diagnostics_active then
+        vim.g.diagnostics_active = false
+        vim.diagnostic.enable(false)
+    else
+        vim.g.diagnostics_active = true
+        vim.diagnostic.enable()
+    end
+end
+
+lvim.builtin.which_key.mappings["lh"]  = { "<cmd>lua Toggle_diagnostics()<CR>", "Toggle Diagnostics" }
+
+
+
+
